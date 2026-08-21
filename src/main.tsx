b77fe385929c4eb0c8593f281834e4f8817bfcfd
Adrian Torres Graña <adriangrana@gmail.com>
2026-08-21 16:25:13 +0200

    style: load application overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './reader-overrides.css';
+import './app-overrides.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
